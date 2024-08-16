b5804dbc55f778508c916a711c5f9f5d640af706
N4palm1337 <mbulatitskiy@gmail.com>
2024-08-16 13:24:43 +0300

    rewrote to async instead of threads

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import telebot, threading, time
+import time, asyncio
 import message_parser
+from telebot.async_telebot import AsyncTeleBot
 from dotenv import load_dotenv
 from os import getenv
 from tinydb import TinyDB, where, operations
@@ -34,38 +35,38 @@ add_chore_success = message_parser.read_message_text('add_chore_success')
 
 DELAY = 10
 
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
+bot = AsyncTeleBot(TOKEN, parse_mode=None)
 
 uninit = TinyDB('db/uninit.json')
 
 @bot.message_handler(commands=['start'])
-def introduce(message):
-    bot.reply_to(message, start_message)
+async def introduce(message):
+    await bot.reply_to(message, start_message)
 
 @bot.message_handler(commands=['info'])
-def give_info(message):
+async def give_info(message):
     if message.from_user.id != DAD and message.from_user.id != MAX:
-        bot.reply_to(message, info_message_child)
+        await bot.reply_to(message, info_message_child)
     else:
-        bot.reply_to(message, info_message_parent, parse_mode='MarkdownV2')
+        await bot.reply_to(message, info_message_parent, parse_mode='MarkdownV2')
 
 @bot.message_handler(commands=['addchore'])
-def add_chore(message): 
+async def add_chore(message): 
     # checking access rights
     if message.from_user.id != DAD and message.from_user.id != MAX:
-        bot.reply_to(message, add_chore_failure_no_access)
+        await bot.reply_to(message, add_chore_failure_no_access)
         # print(message.from_user.id)
     
     # breaking message into lines and checking time format
     try:
         args = message_parser.parse_add_chore(message)
     except:
-        bot.reply_to(message, add_chore_failure_format)
+        await bot.reply_to(message, add_chore_failure_format)
         return
     
     # checking child name
     if args[0] not in ['MAX', 'STE', 'KSU', 'VAL']:
-        bot.reply_to(message, add_chore_failure_child_name)
+        await bot.reply_to(message, add_chore_failure_child_name)
         return
     else:
         designated_child = name_to_id[args[0]]
@@ -78,36 +79,32 @@ def add_chore(message):
     with open('db/id_counter', 'w') as f:
         f.write(str(_id_ + 1))
     
-    bot.reply_to(message, add_chore_success + str(_id_))
+    await bot.reply_to(message, add_chore_success + str(_id_))
 
 
-def send_reminder():
+async def send_reminder():
     while True:
         tasks = uninit.search(where('time') <= time.time())
         
         for task in tasks:
-            bot.send_message(task['to'], task['desc'])
-            bot.send_message(DAD, f"sent reminder to {task['to']} about task with id {task['id']}")
+            await bot.send_message(task['to'], task['desc'])
+            await bot.send_message(DAD, f"sent reminder to {task['to']} about task with id {task['id']}")
             if task['cron'] is not None:
                 uninit.update(operations.set('time', (croniter(task['cron'], 
                                                                datetime.now(tz=timezone('Europe/Moscow')))).get_next()), where('id') == task['id'])
             else:
                 uninit.remove(where('id') == task['id'])
         
-        time.sleep(DELAY)
-        
+        await asyncio.sleep(DELAY)
 
 
-reminder_thread = threading.Thread(target=send_reminder)
-reminder_thread.start()
+async def main():
+    fetch_task = asyncio.create_task(bot.infinity_polling())
+    send_task = asyncio.create_task(send_reminder())
+    
+    await fetch_task
+    await send_task
 
 
-while True:
-    try:
-        bot.infinity_polling()
-    except:
-        time.sleep(10)
-        
-
-
-# async telebot comment modification
+if __name__ == '__main__':
+    asyncio.run(main())
